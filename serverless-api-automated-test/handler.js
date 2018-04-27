10fac42469c6e4792036295b919a4e671dbf6696
Bilger Yahov <bayahov1@gmail.com>
2018-04-27 14:04:40 +0200

    Hi guys, Bilger here.

--- serverless-api-automated-test/handler.js
+++ serverless-api-automated-test/handler.js
@@ -5,7 +5,7 @@ const handlerImplementations = require('./handlerImplementations');
 module.exports.hello = (event, context, callback) => {
 	if (event.httpMethod === 'GET') {
 		return handlerImplementations.helloGET(event, context, callback);
-	} else if (event.httpMehod === 'POST') {
+	} else if (event.httpMethod === 'POST') {
 		return handlerImplementations.helloPOST(event, context, callback);
 	}
 };
@@ -13,7 +13,7 @@ module.exports.hello = (event, context, callback) => {
 module.exports.cheers = (event, context, callback) => {
 	if (event.httpMethod === 'PUT') {
 		return handlerImplementations.cheersPUT(event, context, callback);
-	} else if (event.httpMehod === 'DELETE') {
+	} else if (event.httpMethod === 'DELETE') {
 		return handlerImplementations.cheersDELETE(event, context, callback);
 	}
 };
